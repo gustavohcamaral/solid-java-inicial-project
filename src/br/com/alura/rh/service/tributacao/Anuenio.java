49247a4e698e9ce52c8a13c90c9710f99f8398a6
Gustavo Amaral <gustavohcamaral@gmail.com>
2022-07-12 11:52:59 -0300

    first commit SOLID study

---
+++ src/br/com/alura/rh/service/tributacao/Anuenio.java
@@ -0,0 +1,19 @@
+package br.com.alura.rh.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Anuenio implements Reajuste{
+    private BigDecimal valor;
+    private LocalDate data;
+
+    @Override
+    public BigDecimal valor() {
+        return valor;
+    }
+
+    @Override
+    public LocalDate data() {
+        return data;
+    }
+}
